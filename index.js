48fabd1f831939058a6b57b97e532ff01d148706
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2020-09-12 20:00:06 +0300

    replace method javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+//1.replaces all instances of the search parameter
+
+function replaceString(word, search, replaceWith) {
+    return word.replace(search, replaceWith);
+}
+console.log(replaceString("Susaa Wangari", "Wangari", "Susan"));
+
